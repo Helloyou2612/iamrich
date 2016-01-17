c1b1840ba4d47b008bf10f8ca11f0911b000aa7c
Lam Man <lamman2612@outlook.com>
2016-01-17 20:17:27 +0700

    Initial Commit

---
+++ IAmRich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAmRich
+//
+//  Created by Man Ngoc Lam on 1/17/16.
+//  Copyright © 2016 Man Ngoc Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IAmRich/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IAmRich
+//
+//  Created by Man Ngoc Lam on 1/17/16.
+//  Copyright © 2016 Man Ngoc Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
